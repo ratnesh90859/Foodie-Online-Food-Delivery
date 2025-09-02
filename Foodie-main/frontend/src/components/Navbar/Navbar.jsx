001741c9de689119a7e39f6d1fd6ec7202ae8b10
Ratnesh Singh <ratnesh90859@gmail.com>
2025-09-03 01:39:30 +0530

    updated

--- Foodie-main/frontend/src/components/Navbar/Navbar.jsx
+++ Foodie-main/frontend/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import PropTypes from "prop-types";
 import { StoreContext } from "./context/StoreContext";
 
 const Navbar = ({ setShowLogin }) => {
-  const [menu, setMenu] = useState("menu");
+  const [menu, setMenu] = useState("home");
   const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
 
   const navigate = useNavigate();
